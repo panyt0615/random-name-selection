53b96aa8bb3de454ed451dfa9e275a137dcf38c4
panyt0615 <111557653+panyt0615@users.noreply.github.com>
2025-11-09 15:44:12 +0800

    Add files via upload

---
+++ cloud_config.js
@@ -0,0 +1,9 @@
+// 演示云端配置文件
+window.CLOUD_CONFIG = {
+  "version": "1.0.0",
+  "people": [
+    { "id": 1, "name": "小美", "weight": 1.0, "enabled": true },
+    { "id": 2, "name": "小帅", "weight": 1.0, "enabled": true },
+    { "id": 3, "name": "小明", "weight": 2.0, "enabled": true }
+  ]
+};
